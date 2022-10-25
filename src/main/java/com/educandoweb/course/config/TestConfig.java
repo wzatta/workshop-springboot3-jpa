cdf6866d8f2573538c2bca982cbf0541988d16c8
Waldyr Zatta <wzjssa@gmail.com>
2022-10-25 11:01:57 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -22,8 +22,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u3 = new User(null, "Waldyr Zatta", "eu@gmail.com", "977887777", "123");
 		
-		userRepository.saveAll(Arrays.asList(u1,u2));
+		userRepository.saveAll(Arrays.asList(u1,u2,u3));
 		
 	}
 }
