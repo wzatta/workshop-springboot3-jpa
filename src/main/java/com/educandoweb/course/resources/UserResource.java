e4c2fe28697bab9ebad9b8814a183bf747478c0f
Waldyr Zatta <wzjssa@gmail.com>
2022-10-26 15:51:54 -0300

    User Delete

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,7 +46,11 @@ public class UserResource {
 		//return ResponseEntity.ok().body(obj); este retorna codigo 200
 	}
 	
-	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 	
 	
 }
